3e15101353ed746ea2de59d710ede87732df8bb4
Jordan Swan <jordan.k.swan@live.com>
2020-08-29 15:34:56 -0600

    Added scss bootstrap. Added gulp. Created a dark theme. Added additional content to the experience page.

--- JKSwan_Website/JKSwan_Website/Controllers/HomeController.cs
+++ JKSwan_Website/JKSwan_Website/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using JKSwan_Website.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace JKSwan_Website.Controllers
 {
